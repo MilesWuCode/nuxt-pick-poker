9ca59e8a0b421e3d736a80a174e3d02ffa80b832
miles <slion0725@gmail.com>
2026-01-09 12:05:18 +0800

    upd

---
+++ app/components/HoverCircle.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="circle-wrapper">
+    <a class="circle-hover">
+      <span>查看網站</span>
+    </a>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.circle-wrapper {
+  border-radius: 50%;
+  background-color: #fff;
+}
+
+.circle-hover {
+  width: 150px;
+  height: 150px;
+  align-items: center;
+  color: #000;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  transition: transform 0.3s;
+}
+
+.circle-hover:hover {
+  color: white;
+  transform: scale(1.3);
+}
+
+.circle-hover:before {
+  background-color: #000;
+  border-radius: 50%;
+  content: "";
+  height: 90%;
+  position: absolute;
+  transform: scale(0);
+  transition: transform 0.3s;
+  width: 90%;
+  z-index: -1;
+}
+
+.circle-hover:hover:before {
+  transform: scale(0.95);
+}
+
+.circle-hover:after {
+  border-radius: 50%;
+  content: "";
+  height: 90%;
+  position: absolute;
+  transition: transform 0.3s;
+  width: 90%;
+  background-color: #fff;
+  transform: scale(1);
+  z-index: -2;
+}
+
+.circle-hover:hover:after {
+  background-color: #fff;
+  transform: scale(1);
+  z-index: -2;
+}
+</style>
---
+++ app/pages/hover.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-gray-500 h-screen flex justify-center items-center">
+    <HoverCircle />
+  </div>
+</template>
